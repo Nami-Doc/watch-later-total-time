0f00c68ef07e66846eb6ae588b43a670d86f117e
Nami-Doc <vendethiel@hotmail.fr>
2013-07-12 10:50:53 +0200

    improvements

--- wltt.user.js
+++ wltt.user.js
@@ -1,23 +1,11 @@
 // ==UserScript==
 // @name Watch Later Total Time
-// @description UserScript for YouTube
+// @description UserScript for YouTu
 // @match https://www.youtube.com/feed/watch_later
 // @author Nami-D0C
 // @version 1
 // ==/UserScript==
 
-var totalTime = 0,
-	attr = 'data-context-item-time',
-	els = document.querySelectorAll('[' + attr + ']');
-[].forEach.call(els, function (el) {
-	var val = el.getAttribute(attr).split(':');
-	totalTime += 60 * val[0]; totalTime += +val[1];
-});
-var seconds = totalTime % 60;
-totalTime = Math.floor(totalTime / 60);
-var minutes = totalTime % 60;
-totalTime = Math.floor(totalTime / 60)
-var hours = totalTime % 60;
 
 var id = 'watch-later-total-time',
 	span = document.getElementById(id);
@@ -27,5 +15,27 @@ if (!span) {
 	var nav = document.querySelector('.epic-nav-item-heading');
 	nav.appendChild(span);
 }
-span.innerHTML = ". Total time is " + hours + "h" + minutes
-	+ "m" + seconds + "s";
+
+document.getElementById('feed-item-remove-videos')
+	.addEventListener('click', function () {
+		setTimeout(updateTime, 1000);
+	});
+
+function updateTime() {
+	var totalTime = 0,
+		attr = 'data-context-item-time',
+		els = document.querySelectorAll('[' + attr + ']');
+	[].forEach.call(els, function (el) {
+		var val = el.getAttribute(attr).split(':');
+		totalTime += 60 * val[0];
+		totalTime += +val[1];
+	});
+	var seconds = totalTime % 60;
+	totalTime = Math.floor(totalTime / 60);
+	var minutes = totalTime % 60;
+	totalTime = Math.floor(totalTime / 60)
+	var hours = totalTime % 60;
+	span.innerHTML = ". Total time is " + hours + "h" + minutes
+		+ "m" + seconds + "s";
+}
+updateTime()
